303368da3e8c450ed6edc10db0432ed556a4d8c0
nikolaiser <mail@nikolaiser.com>
2026-01-25 16:42:33 +0100

    chore: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,79 @@
+{
+  description = "Kent";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    make-shell.url = "github:nicknovitski/make-shell";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+  };
+
+  outputs =
+    inputs@{
+      flake-parts,
+      make-shell,
+      rust-overlay,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        make-shell.flakeModules.default
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          pkgs,
+          system,
+          lib,
+          ...
+        }:
+        let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+
+          rustPackage = pkgs.rust-bin.stable.latest.minimal;
+
+          brichka =
+            (pkgs.makeRustPlatform {
+              cargo = rustPackage;
+              rustc = rustPackage;
+            }).buildRustPackage
+              {
+                inherit (cargoToml.package) name version;
+                src = ./.;
+                cargoLock.lockFile = ./Cargo.lock;
+                nativeBuildInputs = [
+                  rustPackage
+                  pkgs.pkg-config
+                ];
+                buildInputs = [
+                  pkgs.openssl
+                ];
+              };
+        in
+        {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
+          };
+
+          make-shells = {
+            default = {
+              buildInputs = [ rustPackage ];
+            };
+
+            test.buildInputs = [ brichka ];
+          };
+
+          packages = {
+            inherit brichka;
+            default = brichka;
+          };
+        };
+    };
+}
